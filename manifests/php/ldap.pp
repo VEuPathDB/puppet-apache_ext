d4fcee0b18d45527fb8d7c6f96f8819697d90d8f
Mustafa Nural <mvnural@gmail.com>
2025-06-23 12:41:36 -0400

    Fix typo.

--- manifests/php/ldap.pp
+++ manifests/php/ldap.pp
@@ -1,5 +1,5 @@
 class apache_ext::php::ldap {
-  ::apache_ext::php::ext { 'ldap': 
-    require => Class['::apache_ext::php'],
+  ::apache_ext::php::ext { 'ldap':
+    require => Class['::apache_ext::php_fpm'],
   }
 }
